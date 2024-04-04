70441c777f3f5209d4a09ae506ccff371fb23439
Truong Van Khai <89502589+VanKhaiii@users.noreply.github.com>
2024-04-04 17:46:58 +0700

    add predict & eval file

---
+++ KGAT-pytorch/predict_kgat.py
@@ -0,0 +1,93 @@
+import os
+import sys
+import random
+from time import time
+
+import pandas as pd
+from tqdm import tqdm
+import torch.nn as nn
+import torch.optim as optim
+
+from model.KGAT import KGAT
+from parser.parser_kgat import parse_kgat_args
+from utils.log_helper import *
+from utils.metrics import *
+from utils.model_helper import *
+from data_loader.loader_kgat import DataLoaderKGAT
+
+
+def evaluate(model, dataloader, Ks, device, save_cf_scores=False):
+    test_batch_size = dataloader.test_batch_size
+    train_user_dict = dataloader.train_user_dict
+    test_user_dict = dataloader.test_user_dict
+
+    model.eval()
+
+    user_ids = list(test_user_dict.keys())
+    user_ids_batches = [user_ids[i: i + test_batch_size] for i in range(0, len(user_ids), test_batch_size)]
+    user_ids_batches = [torch.LongTensor(d) for d in user_ids_batches]
+
+    n_items = dataloader.n_items
+    item_ids = torch.arange(n_items, dtype=torch.long).to(device)
+    
+    cf_scores = []
+    metric_names = ['precision', 'recall', 'ndcg']
+    metrics_dict = {k: {m: 0 for m in metric_names} for k in Ks}
+
+    with tqdm(total=len(user_ids_batches), desc='Evaluating Iteration') as pbar:
+        for bid, batch_user_ids in enumerate(user_ids_batches):
+            batch_user_ids = batch_user_ids.to(device)
+
+            with torch.no_grad():
+                batch_scores = model(batch_user_ids, item_ids, mode='predict')       # (n_batch_users, n_items)
+
+            batch_scores = batch_scores.cpu()
+            batch_metrics = calc_metrics_at_k(batch_scores, train_user_dict, test_user_dict, batch_user_ids.cpu().numpy(), item_ids.cpu().numpy(), Ks)
+
+            # print('====== lksdjflkd ======')
+            # print(batch_scores.shape)
+
+            if save_cf_scores:
+                np.save(f'{args.save_dir}/cf_scores_{bid}.npy', batch_scores.numpy())
+            
+            cf_scores.append(batch_scores.numpy())
+            for k in Ks:
+                for m in metric_names:
+                    # print(k, m, len(batch_metrics[k][m]))
+        
+                    metrics_dict[k][m] += batch_metrics[k][m].sum()
+            pbar.update(1)
+
+    cf_scores = np.concatenate(cf_scores, axis=0)
+    for k in Ks:
+        for m in metric_names:
+            metrics_dict[k][m] = metrics_dict[k][m] / len(user_ids)
+    return cf_scores, metrics_dict
+
+def predict(args):
+    # GPU / CPU
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # load data
+    data = DataLoaderKGAT(args, logging)
+
+    # load model
+    model = KGAT(args, data.n_users, data.n_entities, data.n_relations)
+    model = load_model(model, args.pretrain_model_path)
+    model.to(device)
+
+    # predict
+    Ks = eval(args.Ks)
+    k_min = min(Ks)
+    k_max = max(Ks)
+
+    cf_scores, metrics_dict = evaluate(model, data, Ks, device, save_cf_scores=True)
+    np.save(args.save_dir + 'cf_scores.npy', cf_scores)
+    print('CF Evaluation: Precision [{:.4f}, {:.4f}], Recall [{:.4f}, {:.4f}], NDCG [{:.4f}, {:.4f}]'.format(
+        metrics_dict[k_min]['precision'], metrics_dict[k_max]['precision'], metrics_dict[k_min]['recall'], metrics_dict[k_max]['recall'], metrics_dict[k_min]['ndcg'], metrics_dict[k_max]['ndcg']))
+
+
+
+if __name__ == '__main__':
+    args = parse_kgat_args()
+    predict(args)
