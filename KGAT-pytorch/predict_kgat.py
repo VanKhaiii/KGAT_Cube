94d5686cf28856d0f3cd00d87dbb4f82d152c756
Truong Van Khai <89502589+VanKhaiii@users.noreply.github.com>
2024-04-04 18:36:18 +0700

    add predict & eval file 3

--- KGAT-pytorch/predict_kgat.py
+++ KGAT-pytorch/predict_kgat.py
@@ -74,6 +74,9 @@ def predict(args):
     k_max = max(Ks)
 
     cf_scores, metrics_dict = evaluate(model, data, Ks, device)
+    
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
     np.save(args.save_dir + 'cf_scores.npy', cf_scores)
     print('CF Evaluation: Precision [{:.4f}, {:.4f}], Recall [{:.4f}, {:.4f}], NDCG [{:.4f}, {:.4f}]'.format(
         metrics_dict[k_min]['precision'], metrics_dict[k_max]['precision'], metrics_dict[k_min]['recall'], metrics_dict[k_max]['recall'], metrics_dict[k_min]['ndcg'], metrics_dict[k_max]['ndcg']))
