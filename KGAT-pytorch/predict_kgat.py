67fd151d54b3fc1b39f0964c2dc3c89c42fc2673
Truong Van Khai <89502589+VanKhaiii@users.noreply.github.com>
2024-04-04 18:18:09 +0700

    add predict & eval file 2

--- KGAT-pytorch/predict_kgat.py
+++ KGAT-pytorch/predict_kgat.py
@@ -9,14 +9,14 @@ import torch.nn as nn
 import torch.optim as optim
 
 from model.KGAT import KGAT
-from parser.parser_kgat import parse_kgat_args
+from parser.parser_kgat import *
 from utils.log_helper import *
 from utils.metrics import *
 from utils.model_helper import *
 from data_loader.loader_kgat import DataLoaderKGAT
 
 
-def evaluate(model, dataloader, Ks, device, save_cf_scores=False):
+def evaluate(model, dataloader, Ks, device):
     test_batch_size = dataloader.test_batch_size
     train_user_dict = dataloader.train_user_dict
     test_user_dict = dataloader.test_user_dict
@@ -29,13 +29,13 @@ def evaluate(model, dataloader, Ks, device, save_cf_scores=False):
 
     n_items = dataloader.n_items
     item_ids = torch.arange(n_items, dtype=torch.long).to(device)
-    
+
     cf_scores = []
     metric_names = ['precision', 'recall', 'ndcg']
-    metrics_dict = {k: {m: 0 for m in metric_names} for k in Ks}
+    metrics_dict = {k: {m: [] for m in metric_names} for k in Ks}
 
     with tqdm(total=len(user_ids_batches), desc='Evaluating Iteration') as pbar:
-        for bid, batch_user_ids in enumerate(user_ids_batches):
+        for batch_user_ids in user_ids_batches:
             batch_user_ids = batch_user_ids.to(device)
 
             with torch.no_grad():
@@ -44,24 +44,16 @@ def evaluate(model, dataloader, Ks, device, save_cf_scores=False):
             batch_scores = batch_scores.cpu()
             batch_metrics = calc_metrics_at_k(batch_scores, train_user_dict, test_user_dict, batch_user_ids.cpu().numpy(), item_ids.cpu().numpy(), Ks)
 
-            # print('====== lksdjflkd ======')
-            # print(batch_scores.shape)
-
-            if save_cf_scores:
-                np.save(f'{args.save_dir}cf_scores_{bid}.npy', batch_scores.numpy())
-            
             cf_scores.append(batch_scores.numpy())
             for k in Ks:
                 for m in metric_names:
-                    # print(k, m, len(batch_metrics[k][m]))
-        
-                    metrics_dict[k][m] += batch_metrics[k][m].sum()
+                    metrics_dict[k][m].append(batch_metrics[k][m])
             pbar.update(1)
 
     cf_scores = np.concatenate(cf_scores, axis=0)
     for k in Ks:
         for m in metric_names:
-            metrics_dict[k][m] = metrics_dict[k][m] / len(user_ids)
+            metrics_dict[k][m] = np.concatenate(metrics_dict[k][m]).mean()
     return cf_scores, metrics_dict
 
 def predict(args):
@@ -75,13 +67,13 @@ def predict(args):
     model = KGAT(args, data.n_users, data.n_entities, data.n_relations)
     model = load_model(model, args.pretrain_model_path)
     model.to(device)
-    
+
     # predict
     Ks = eval(args.Ks)
     k_min = min(Ks)
     k_max = max(Ks)
 
-    cf_scores, metrics_dict = evaluate(model, data, Ks, device, save_cf_scores=True)
+    cf_scores, metrics_dict = evaluate(model, data, Ks, device)
     np.save(args.save_dir + 'cf_scores.npy', cf_scores)
     print('CF Evaluation: Precision [{:.4f}, {:.4f}], Recall [{:.4f}, {:.4f}], NDCG [{:.4f}, {:.4f}]'.format(
         metrics_dict[k_min]['precision'], metrics_dict[k_max]['precision'], metrics_dict[k_min]['recall'], metrics_dict[k_max]['recall'], metrics_dict[k_min]['ndcg'], metrics_dict[k_max]['ndcg']))
@@ -91,3 +83,4 @@ def predict(args):
 if __name__ == '__main__':
     args = parse_kgat_args()
     predict(args)
+
