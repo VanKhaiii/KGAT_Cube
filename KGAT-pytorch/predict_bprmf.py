d932369af365ae805b386dc1500d5691b1b80cd4
Truong Van Khai <89502589+VanKhaiii@users.noreply.github.com>
2024-04-26 07:42:50 +0700

    test

--- KGAT-pytorch/predict_bprmf.py
+++ KGAT-pytorch/predict_bprmf.py
@@ -76,9 +76,11 @@ def predict(args):
 
     cf_scores, metrics_dict = evaluate(model, data, Ks, device)
     
+    
+    
     if not os.path.exists(args.save_dir):
         os.makedirs(args.save_dir)
-    np.save(args.save_dir + 'cf_scores.npy', cf_scores)
+    np.save('cf_scores.npy', cf_scores)
     print('CF Evaluation: Precision [{:.4f}, {:.4f}], Recall [{:.4f}, {:.4f}], NDCG [{:.4f}, {:.4f}]'.format(
         metrics_dict[k_min]['precision'], metrics_dict[k_max]['precision'], metrics_dict[k_min]['recall'], metrics_dict[k_max]['recall'], metrics_dict[k_min]['ndcg'], metrics_dict[k_max]['ndcg']))
 
